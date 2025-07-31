c3fb284d0d8eacade145c486450ff5992abec3f0
Brunopzn <brunaohp@gmail.com>
2025-07-31 17:09:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const main = document.querySelector('.main');
+const box = document.querySelector('.box');
+const textContent = document.querySelector('.textContent')
+const h3 = document.querySelector('h3')
+
+
+
+async function frases() {
+
+    const res = await fetch('frases.json');
+    const dados = await res.json();
+    let contador = 0;
+    const frasesRandom = dados.sort(() => 0.5 - Math.random())
+
+    frasesRandom.map(frase => {
+        main.appendChild(box);
+        box.appendChild(textContent);
+        textContent.appendChild(h3);
+        h3.innerHTML = frase;
+
+    })
+
+
+}
+
+frases()
+
+
+function gerarFrase() {
+
+    frases()
+
+
+}
+
